d8f51ac583f9a40b2f07c251a47a83a25254d70d
Putt <junrai82@gmail.com>
2017-08-22 14:27:13 +0700

    move to use Wrapper instead

--- src/index.js
+++ src/index.js
@@ -1,61 +1,15 @@
 import seneca from 'seneca';
-import Promise from 'bluebird';
 import {register} from './register';
 import * as Mesh from './plugin/mesh';
 import * as Module from "./module";
-
-const serializeError = (e)=> {
-  return {name: e.name, stack: e.stack, message: e.message, errors: e.errors};
-}
-
-const deserializeError = (oe)=> {
-  const e = new Error(oe.message);
-  e.name = oe.name;
-  e.errors = oe.errors;
-  e.stack = oe.stack;
-  return e;
-}
+import * as Wrapper from './seneca-wrapper';
 
 export default function (senecaOptions, transportConfig = {}) {
   const si = seneca(Module.parseOption(senecaOptions));
 
-  const act = (args) => {
-    return new Promise((resolve, reject)=> {
-      si.act(args, function (err, resp) {
-        if (err) {
-          reject(err);
-          return;
-        }
-        else if (!resp.ok) reject(deserializeError(resp.error));
-        else resolve(resp.result);
-      });
-    });
-  }
+  const act = Wrapper.actGenerator(si)
 
-  const add = (options, callback)=> {
-    si.add(options, function (args, done) {
-      try {
-        if (callback.length >= 2) {
-          callback.bind(si)(args, done);
-        } else {
-          const resultWithArgs = callback.bind(si)(args);
-          const isThenable = ('function' === typeof resultWithArgs.then);
-          if (isThenable) {
-            resultWithArgs.then((response)=> {
-              done(null, {ok: true, result: response});
-            }).catch((e)=> {
-              done(null, {ok: false, error: serializeError(e)});
-            });
-          } else {
-            done(null, {ok: true, result: resultWithArgs});
-          }
-        }
-      } catch (e) {
-        done(null, {ok: false, error: serializeError(e)});
-        return;
-      }
-    })
-  }
+  const add = Wrapper.addGenerator(si)
 
   switch (transportConfig.type) {
     case 'mesh':
